39d6b150ee24322f5868f0183671ec2a39776649
r00cha <roocha99pt@gmail.com>
2022-12-11 23:14:00 +0000

    Changed rock colors and controller

--- src/main/java/com/ldts/frogger/controller/game/RockController.java
+++ src/main/java/com/ldts/frogger/controller/game/RockController.java
@@ -30,11 +30,12 @@ public class RockController extends GameController{
     private void moveRock(Rock rock) {
         Position oldPosition = rock.getPosition();
         Position newPosition = oldPosition;
-        if(rock.getDirection() == 1 && rock.getPosition().getX() > 20){
+
+        if(rock.getDirection() == 1 && rock.getPosition().getX() > 19){
             newPosition = new Position(0, rock.getPosition().getY());
         }
         else if (rock.getDirection() == 0 && rock.getPosition().getX() < 0){
-            newPosition = new Position(20, rock.getPosition().getY());
+            newPosition = new Position(19, rock.getPosition().getY());
         }
 
         else if(rock.getDirection() == 1){
@@ -43,9 +44,9 @@ public class RockController extends GameController{
         else if(rock.getDirection() == 0){
             newPosition = rock.getPosition().getLeft();
         }
-        rock.setPosition(newPosition);
         if(getModel().getFrog().getPosition().equals(oldPosition)) {
             getModel().getFrog().setPosition(newPosition);
         }
+        rock.setPosition(newPosition);
     }
 }
--- src/main/java/com/ldts/frogger/viewer/game/RockViewer.java
+++ src/main/java/com/ldts/frogger/viewer/game/RockViewer.java
@@ -6,6 +6,6 @@ import com.ldts.frogger.model.game.elements.Rock;
 public class RockViewer implements ElementViewer<Rock>{
     @Override
     public void draw(Rock element, GUI gui) {
-        gui.drawText(element.getPosition(),"Ļ","#2d2d36","#4e535a");
+        gui.drawText(element.getPosition(),"╓","#4e535a","#2d2d36");
     }
 }
