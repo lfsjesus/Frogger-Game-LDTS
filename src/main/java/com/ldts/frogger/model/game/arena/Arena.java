ced9dac9ae2edc6e08b216420460fbeba8b1e6e5
Luís Jesus <luisjesus__2003@outlook.com>
2022-12-15 14:37:01 +0000

    some minor fixes

--- src/main/java/com/ldts/frogger/model/game/arena/Arena.java
+++ src/main/java/com/ldts/frogger/model/game/arena/Arena.java
@@ -9,7 +9,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Arena {
-    public static int level = 0;
+    public static int level = 1;
     private final int width;
     private final int height;
     private static int points = 0;
--- src/main/java/com/ldts/frogger/model/game/elements/Car.java
+++ src/main/java/com/ldts/frogger/model/game/elements/Car.java
@@ -3,33 +3,16 @@ package com.ldts.frogger.model.game.elements;
 import com.ldts.frogger.model.Position;
 
 public class Car extends MoveableElement{
-    private int direction = 0;
-    private String color;
     public Car(int x, int y) {
         super(x, y);
     }
 
     public Car(Position position, int direction, String color) {
-        super(position.getX(), position.getY());
-        this.direction = direction;
-        this.color = color;
+        super(position, direction, color);
     }
 
     public Car(Position position, int direction) {
-        super(position.getX(), position.getY());
-
-        this.direction = direction;
+        super(position, direction);
     }
 
-    public int getDirection() {
-        return direction;
-    }
-
-    public void setDirection(int direction) {
-        this.direction = direction;
-    }
-
-    public String getColor() {
-        return this.color;
-    }
 }
